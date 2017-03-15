555c8815e06be7882f1cf409552a2080caa76924
Dongbin Kim <bindongkim@gmail.com>
2017-03-15 23:12:18 +0900

    주석 정리

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 /*
-  WorkFlow
+  Customer WorkFlow
   0. 해당 웹페이지(소비자화면)는 이미 떠있는 상태(예시화면 필요)
   1. Net module를 통해 아두이노에서 data를 받음
   2. 'data' 이벤트의 Callback으로 io.emit('data-received')실행 - 콜백으로 data(json형태)보냄
   3. Client단에서 socket.on('data-received')을 받아서 넘어온 json데이터를 웹페이지에서 세팅
+
+  Manager WorkFlow
 */
 
 var express = require('express');
@@ -23,7 +25,7 @@ var pool = mysql.createPool({
 
 /*
   1. DB에서 미리 데이터를 가져와서 Map에 세팅한다.
-  2. Browser와의 데이터교환은 Map을 통해서만 한다.
+  2. Browser와의 데이터교환은 productObj을 통해서만 한다.
 */
 var productMap = new Map(); // DB에서 가져올 Product정보들을 담기위한 Map
 var productObj = new Object(); // Client로 Product정보를 담아보낼 Object
@@ -61,7 +63,6 @@ function mapIntoObject(map) {
 // Net module을 통한 서버 instance생성
 var netServer = net.createServer(function(netSocket) {
   // client(아두이노)로부터 data이벤트 발생시
-  // 아두이노 와이파이 접속시 상품 초기갯수부터 넘어오도록 되어있음(data: 0_2_X)
   netSocket.on('data', function(data) {
     console.log('data:' + data);
     // 아두이노에서 오는 data 형식: id_갯수_X/O(X:올릴때, O:내릴때) ex: 0_1_O
@@ -82,8 +83,6 @@ var netServer = net.createServer(function(netSocket) {
   netSocket.on('close', function() {
     console.log('client disconnted.');
   });
-  // client가 접속하면 화면에 출력해주는 메시지(클라이언트에게 보여지는 메시지)
-  // socket.write('welcome to server');
 });
 
 // 에러가 발생할 경우 화면에 에러메시지 출력
