9dbf6099107d203124cd9aa1c5046c9d712726ba
Dongbin Kim <bindongkim@gmail.com>
2017-03-15 23:13:15 +0900

    서버에서 받아온 product정보를 가지고 DOM을 그려서 container div안에 넣어줌

--- public/js/customer.js
+++ public/js/customer.js
@@ -1,4 +1,23 @@
 var socket = io.connect('http://localhost:3000');
 socket.on('data-received', function(data) {
-  console.log('data: ' + data.name + ", " + data.brand);
+  var productInfoDOM = '<div class="row content">'
+                     +   '<div class="col-md-6">' + '<img src="../images/' + data.image + '.jpg" class="img-responsive image-rounded">' + '</div>'
+                     +   '<div class="col-md-6">'
+                     +     '<div class="panel panel-default">'
+                     +       '<div class="panel-heading">' + '상품 정보' + '</div>'
+                     +       '<table class="table table-hover table-bordered">'
+                     +         '<tr>'
+                     +           '<th>' + '상품 브랜드' + '</th>'
+                     +           '<td>' + data.brand + '</td>'
+                     +         '</tr>'
+                     +         '<tr>'
+                     +           '<th>' + '상품 이름' + '</th>'
+                     +           '<td>' + data.name + '</td>'
+                     +         '</tr>'
+                     +       '</table>'
+                     +     '</div>'
+                     +   '</div>'
+                     + '</div>';
+  var container = document.getElementsByClassName('container');
+  container[0].innerHTML = productInfoDOM;
 });
