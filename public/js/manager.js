8943ca2efa02de814fc25d3555014372887c097b
Dongbin Kim <bindongkim@gmail.com>
2017-03-16 00:51:32 +0900

    관리자화면이 처음 렌더링되면 전달받은 상품리스트(products)를 화면에 그려줌

--- public/js/manager.js
+++ public/js/manager.js
@@ -1,4 +1,18 @@
 var socket = io.connect('http://localhost:3000');
-socket.on('productCount-updated', function(productCount) {
-  console.log('productCount: ' + productCount);
+socket.on('product-moved', function(product) {
+
+});
+
+socket.on('product-info', function(products) {
+  var productsDOM = '<div class="panel panel-default">'
+                    + '<div class="panel-heading">' + '상품 리스트' + '</div>'
+                    + '<ul class="list-group">';
+  Object.keys(products).forEach(function(productId) {
+    productsDOM += '<li class="list-group-item">'
+    + '<span class="badge">' + products[productId].count + '</span>'
+    + products[productId].name + '</li>';
+  });
+  productsDOM += '</ul></div>';
+  var container = document.getElementsByClassName('container');
+  container[0].innerHTML = productsDOM;
 });
