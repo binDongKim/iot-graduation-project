5c75bf5aa7f1170953d262baf76cf5bfa7d2f139
Dongbin Kim <bindongkim@gmail.com>
2017-03-16 00:50:33 +0900

    - customer.html과 기본 레이아웃은 동일하게 - 전달받는 parameter이름 수정 data->product

--- public/js/customer.js
+++ public/js/customer.js
@@ -1,21 +1,21 @@
 var socket = io.connect('http://localhost:3000');
 var productId = null;
-socket.on('product-moved', function(data) {
-  if(data.pickedUp && productId != data.id) { // 소비자가 '다른' 상품을 '집어들었을때'만
-    productId = data.id;
+socket.on('product-moved', function(product) {
+  if(product.pickedUp && productId != product.id) { // 소비자가 '다른' 상품을 '집어들었을때'만
+    productId = product.id;
     var productInfoDOM = '<div class="row content">'
-                       +   '<div class="col-md-6">' + '<img src="../images/' + data.image + '.jpg" class="img-responsive image-rounded">' + '</div>'
+                       +   '<div class="col-md-6">' + '<img src="../images/' + product.image + '.jpg" class="img-responsive image-rounded">' + '</div>'
                        +   '<div class="col-md-6">'
                        +     '<div class="panel panel-default">'
                        +       '<div class="panel-heading">' + '상품 정보' + '</div>'
                        +       '<table class="table table-hover table-bordered">'
                        +         '<tr>'
                        +           '<th>' + '상품 브랜드' + '</th>'
-                       +           '<td>' + data.brand + '</td>'
+                       +           '<td>' + product.brand + '</td>'
                        +         '</tr>'
                        +         '<tr>'
                        +           '<th>' + '상품 이름' + '</th>'
-                       +           '<td>' + data.name + '</td>'
+                       +           '<td>' + product.name + '</td>'
                        +         '</tr>'
                        +       '</table>'
                        +     '</div>'
