caa65e6d7f4f98a36f1ec159726b9c7fea188f8b
Dongbin Kim <bindongkim@gmail.com>
2017-04-18 01:29:13 +0900

    관리자화면 재구성

--- public/js/manager.js
+++ public/js/manager.js
@@ -4,15 +4,14 @@ socket.on('product-moved', function(product) {
 });
 
 socket.on('product-info', function(products) {
-  var productsDOM = '<div class="panel panel-default">'
-                    + '<div class="panel-heading">' + '상품 리스트' + '</div>'
-                    + '<ul class="list-group">';
-  Object.keys(products).forEach(function(productId) {
-    productsDOM += '<li class="list-group-item">'
-    + '<span id="' + productId + '" class="badge">' + products[productId].count + '</span>'
+  var sectorLis = document.getElementsByClassName('sectors')[0].querySelectorAll('li');
+  var productList = '';
+  Object.keys(products).forEach(function(productId, index) {
+    sectorLis[index].querySelector('span').setAttribute('id', productId);
+    sectorLis[index].querySelector('span').innerHTML = products[productId].count;
+    productList += '<li class="list-group-item">'
+    + '<span class="badge">' + products[productId].countSet + '</span>'
     + products[productId].name + '</li>';
   });
-  productsDOM += '</ul></div>';
-  var container = document.getElementsByClassName('container');
-  container[0].innerHTML = productsDOM;
+  document.getElementById('productList').innerHTML = productList;
 });
